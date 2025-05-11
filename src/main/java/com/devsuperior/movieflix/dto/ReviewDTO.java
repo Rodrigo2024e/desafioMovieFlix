d9fc92eb071820e40b863f6b2525b2908cf1900e
Rodrigo <lucianorodrigocarvalho@gmail.com>
2025-05-11 18:28:35 -0300

    ReviewController OK

--- src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -1,91 +1,100 @@
 package com.devsuperior.movieflix.dto;
 
 import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.projections.ReviewProjection;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class ReviewDTO {
 
-    private Long id;
+	 private Long id;
 
-    @NotBlank(message = "Campo requerido")
-    private String text;
+	    @NotBlank(message = "Campo requerido")
+	    private String text;
 
-    @NotNull(message = "Campo requerido")
-    private Long movieId;    
-    
-    private Long userId;
-    private String userName;
-    private String userEmail;
-    
-     public ReviewDTO() {
-    	
-    }
-    
-	public ReviewDTO(Long id, String text, Long movieId, Long userId, String userName, String userEmail) {
-		this.id = id;
-		this.text = text;
-		this.movieId = movieId;
-		this.userId = userId;
-		this.userName = userName;
-		this.userEmail = userEmail;
-	}
-	
-	public ReviewDTO(Review entity) {
-		id = entity.getId();
-		text = entity.getText();
-		movieId = entity.getMovie().getId();
-		userId = entity.getUser().getId();
-		userName = entity.getUser().getName();
-		userEmail = entity.getUser().getEmail();
-	}
-	
-	public Long getId() {
-		return id;
-	}
+	    @NotNull(message = "Campo requerido")
+	    private Long movieId;    
+	    
+	    private Long userId;
+	    private String userName;
+	    private String userEmail;
+	    
+	    public ReviewDTO() {
+	    }
+	    
+		public ReviewDTO(Long id, String text, Long movieId, Long userId, String userName, String userEmail) {
+			this.id = id;
+			this.text = text;
+			this.movieId = movieId;
+			this.userId = userId;
+			this.userName = userName;
+			this.userEmail = userEmail;
+		}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+		public ReviewDTO(ReviewProjection projection) {
+			id = projection.getId();
+			text = projection.getText();
+			movieId = projection.getMovieId();
+			userId = projection.getUserId();
+			userName = projection.getUserName();
+			userEmail = projection.getUserEmail();
+		}
 
-	public String getText() {
-		return text;
-	}
+		public ReviewDTO(Review entity) {
+			id = entity.getId();
+			text = entity.getText();
+			movieId = entity.getMovie().getId();
+			userId = entity.getUser().getId();
+			userName = entity.getUser().getName();
+			userEmail = entity.getUser().getEmail();
+		}
 
-	public void setText(String text) {
-		this.text = text;
-	}
+		public Long getId() {
+			return id;
+		}
 
-	public Long getMovieId() {
-		return movieId;
-	}
+		public void setId(Long id) {
+			this.id = id;
+		}
 
-	public void setMovieId(Long movieId) {
-		this.movieId = movieId;
-	}
+		public String getText() {
+			return text;
+		}
 
-	public Long getUserId() {
-		return userId;
-	}
+		public void setText(String text) {
+			this.text = text;
+		}
 
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
+		public Long getMovieId() {
+			return movieId;
+		}
 
-	public String getUserName() {
-		return userName;
-	}
+		public void setMovieId(Long movieId) {
+			this.movieId = movieId;
+		}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+		public Long getUserId() {
+			return userId;
+		}
 
-	public String getUserEmail() {
-		return userEmail;
-	}
+		public void setUserId(Long userId) {
+			this.userId = userId;
+		}
 
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
+		public String getUserName() {
+			return userName;
+		}
+
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+		public String getUserEmail() {
+			return userEmail;
+		}
+
+		public void setUserEmail(String userEmail) {
+			this.userEmail = userEmail;
+		}
 }
